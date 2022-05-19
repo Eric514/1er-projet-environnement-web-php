8181bd943e2293f6a2a89678dc0e5577a1dbe180
Eric Charbonnier <102246532+Eric514@users.noreply.github.com>
2022-05-19 17:22:38 -0400

    test retiré

--- password.php
+++ password.php
@@ -3,7 +3,6 @@ function verificationMdp($mdp)
 {   
     $carac = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     $mdp = trim($mdp);
-    $mdp = "nb1";
     for ($i = 0; $i < count($carac); $i++) {
 
         for ($j = 0; $j < count($carac); $j++) {
